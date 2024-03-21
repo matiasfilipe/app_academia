f30d97488fd3e682053ea07d8d452cf097205f67
matiasfilipe <151673357+matiasfilipe@users.noreply.github.com>
2024-03-21 07:36:59 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import {SafeAreaView} from 'react-native'
+
+import {Title} from './components/Title'
+
+import {SubTitle} from './components/SubTitle'
+
+
+export default function App() {
+  return (
+
+    <SafeAreaView>
+
+      <Title/>
+
+      <SubTitle/>
+
+  
+
+    
+
+    </SafeAreaView>
+    
+  )
+}
+
